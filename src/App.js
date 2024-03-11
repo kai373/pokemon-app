47a45a202b7f00acebe95911a520dee8e402c3fd
Minami <kai3730421@gmail.com>
2024-03-11 10:32:02 +0900

    conplete ポケモンのデータを取得

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
-  return (
-    <div className="App">
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
 
-    </div>
-  );
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res)
+    };
+    fetchPokemonData();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
